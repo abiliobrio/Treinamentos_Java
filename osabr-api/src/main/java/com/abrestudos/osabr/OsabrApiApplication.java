2cef1d77c3d40e8e50b29f182218ff3dc8104cd1
abiliobrio <abiliobrio@gmail.com>
2021-01-14 09:08:53 -0300

    Commit do workspace de treinamento e testes

---
+++ osabr-api/src/main/java/com/abrestudos/osabr/OsabrApiApplication.java
@@ -0,0 +1,13 @@
+package com.abrestudos.osabr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsabrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsabrApiApplication.class, args);
+	}
+
+}
---
+++ osabr-api/src/test/java/com/abrestudos/osabr/OsabrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abrestudos.osabr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsabrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
